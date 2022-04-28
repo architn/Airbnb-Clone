17d722282959e68807359fa973cee7fbae2f0a08
Divyesh Darji <darji.di@northeastern.edu>
2022-04-28 14:15:50 -0400

    Property Data Fetching and Updating at DB level

--- src/Views/Events/Events.jsx
+++ src/Views/Events/Events.jsx
@@ -1,33 +1,40 @@
-import EventsCard from '../../components/Cards/Events/EventsCard'
-import Navbar from '../../components/Navbar/Navbar'
-import events from '../../data/events.js'
+import EventsCard from "../../components/Cards/Events/EventsCard";
+import Navbar from "../../components/Navbar/Navbar";
+import events from "../../data/events.js";
 
-const filterRecords = events.filter( (eve) => {
-  return eve.price > 0
-})
+const filterRecords = events.filter((eve) => {
+  return eve.price > 0;
+});
 
-function createEventsCard(events){
+function CreateEventsCard(events) {
   return (
-    <div className='col-3'>
-      <EventsCard title={events.title} imgURL={events.imgURL} price={events.price} rating={events.rating} location={events.location}
-      upvotes={events.upvotes}/>
+    <div className="col-3">
+      <EventsCard
+        title={events.title}
+        imgURL={events.imgURL}
+        price={events.price}
+        rating={events.rating}
+        location={events.location}
+        upvotes={events.upvotes}
+      />
     </div>
-  )
+  );
 }
 
 function Events(props) {
-  
   return (
-    <div className='container'>
+    <div className="container">
       {props.reusability ? null : <Navbar />}
-      <br/>
+      <br />
       <h5>All Experiences</h5>
-      <br/>
-      <div className='row'>
-      {filterRecords.map(createEventsCard)}
+      <br />
+      <div className="row">
+        {filterRecords.map((ev) => (
+          <CreateEventsCard key={ev.id} {...ev} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Events
+export default Events;
--- src/Views/UserCustomView/UserCustomView.jsx
+++ src/Views/UserCustomView/UserCustomView.jsx
@@ -1,107 +1,131 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import PropertyCard from '../../components/Cards/Property/PropertyCard';
-import ReservationCard from '../../components/Cards/Reservation/ReservationCard.jsx';
-import Navbar from '../../components/Navbar/Navbar'
-import Events from '../Events/Events'
-import '../UserCustomView/UserCustomView.css'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import PropertyCard from "../../components/Cards/Property/PropertyCard";
+import ReservationCard from "../../components/Cards/Reservation/ReservationCard.jsx";
+import Navbar from "../../components/Navbar/Navbar";
+import Events from "../Events/Events";
+import "../UserCustomView/UserCustomView.css";
 
-function createPropertiesJumbotron(properties){
-   
-         return <div className='col-3'>
-            <PropertyCard 
-          title={properties.Title} 
-          roomid={properties._id}
-          imgURL ='https://a0.muscache.com/im/pictures/631c8926-9ed6-4226-b5c4-c5452e84e6ba.jpg?im_w=720'
-          street = {properties.Street} 
-          rating = {4.0}
-          reviews = {3.9}
-          price={properties.Price}
-          img1 = {properties.img1}
-          description = {properties.Description}
-          apartmentType={properties.ApartmentType}
-          />
-         </div>
+function CreatePropertiesJumbotron(properties) {
+  return (
+    <div className="col-3">
+      <PropertyCard
+        title={properties.Title}
+        roomid={properties._id}
+        imgURL="https://a0.muscache.com/im/pictures/631c8926-9ed6-4226-b5c4-c5452e84e6ba.jpg?im_w=720"
+        street={properties.Street}
+        rating={4.0}
+        reviews={3.9}
+        price={properties.Price}
+        img1={properties.img1}
+        description={properties.Description}
+        apartmentType={properties.ApartmentType}
+      />
+    </div>
+  );
 }
 
-function createReservationCard(reservation){
-    return(
-        <div className='col-3'>
-            <ReservationCard 
-            reservationID={reservation._id}
-            propertyID = {reservation.property}
-            checkInDate = {reservation.checkInDate}
-            checkOutDate = {reservation.checkOutDate}
-            guests = {reservation.numberOfGuests}
-            price = {reservation.cost}/>
-        </div>
-    )
+function CreateReservationCard(reservation) {
+  return (
+    <div className="col-3">
+      <ReservationCard
+        reservationID={reservation._id}
+        propertyID={reservation.property}
+        checkInDate={reservation.checkInDate}
+        checkOutDate={reservation.checkOutDate}
+        guests={reservation.numberOfGuests}
+        price={reservation.cost}
+      />
+    </div>
+  );
 }
-function createNameElement(user){
-    return <h3>Hi {user.name}!</h3>
+function CreateNameElement(user) {
+  return <h3>Hi {user.name}!</h3>;
 }
-
 
 function UserCustomView() {
-    const [properties, setProperties] = useState([]);
-    const [reservations, setReservations] = useState([]);
-    const [user, setUser] = useState([]);
-    useEffect( () => {
+  const [properties, setProperties] = useState([]);
+  const [reservations, setReservations] = useState([]);
+  const [user, setUser] = useState([]);
+  useEffect(() => {
     axios
-        .get('http://localhost:3002/getPropertyByUser', {
+      .get("http://localhost:3002/getPropertyByUser", {
         withCredentials: true,
-        })
-        .then((response) => {
+      })
+      .then((response) => {
         if (response.status === 200) {
-        setProperties(response.data)
+          console.log(response);
+          setProperties(response.data);
         }
-        })
-        .catch((err) => {
+      })
+      .catch((err) => {
         console.log(err);
-            //setError(err.response.data.msg);
-        });
-    axios.get('http://localhost:3002/getUserDetails', {
+        //setError(err.response.data.msg);
+      });
+    axios
+      .get("http://localhost:3002/getUserDetails", {
         withCredentials: true,
-    })
-    .then( (response) => {
-        if(response.status === 200){
-            setUser(response.data)
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setUser(response.data);
         }
-    });
-    axios.get('http://localhost:3002/getReservationsByUser', {
+      });
+    axios
+      .get("http://localhost:3002/getReservationsByUser", {
         withCredentials: true,
-    })
-    .then( (response) => {
-        if(response.status === 200){
-            setReservations(response.data)
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setReservations(response.data);
         }
-    })
-    }, [properties._id]);
-    
+      });
+  }, [properties._id]);
+
   return (
-    <div className='container'>
-        <Navbar />
-       
-       {user && user.map(createNameElement)}
-        <br/><br/>
-        <div className='row'>
-           <h4>Your Properties ({properties.length})</h4>
-           <br/><br/>
-           {properties.length === 0 ? <div className='nothingReserved'>You have not listed any property yet!</div>: properties.map(createPropertiesJumbotron)}
-        </div>
-        <br/>
-        <div className='row'>
-            <h4>Your Reservations ({reservations.length})</h4>
-            <br/><br/>
-            {reservations.length === 0 ? <div className='nothingReserved'><span classname='msgText'>You have not reserved any property yet</span></div>: reservations.map(createReservationCard)}
-        </div>
-        <br/><br/>
-        <div>
-            <h4>Explore Events</h4>
-            <Events reusability={true}/>
-        </div>
+    <div className="container">
+      <Navbar />
+
+      {user && user.map((u) => <CreateNameElement key={u._id} {...u} />)}
+      <br />
+      <br />
+      <div className="row">
+        <h4>Your Properties ({properties.length})</h4>
+        <br />
+        <br />
+        {properties.length === 0 ? (
+          <div className="nothingReserved">
+            You have not listed any property yet!
+          </div>
+        ) : (
+          properties.map((p) => (
+            <CreatePropertiesJumbotron {...p} key={p._id} />
+          ))
+        )}
+      </div>
+      <br />
+      <div className="row">
+        <h4>Your Reservations ({reservations.length})</h4>
+        <br />
+        <br />
+        {reservations.length === 0 ? (
+          <div className="nothingReserved">
+            <span className="msgText">
+              You have not reserved any property yet
+            </span>
+          </div>
+        ) : (
+          reservations.map((r) => <CreateReservationCard {...r} key={r._id} />)
+        )}
+      </div>
+      <br />
+      <br />
+      <div>
+        <h4>Explore Events</h4>
+        <Events reusability={true} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default UserCustomView
+export default UserCustomView;
