049f2c9a3510ae6bfe5f5339390ffc09c33a3e40
AishwaryaBalyaya10 <balyaya.a@northeastern.edu>
2022-04-29 18:03:49 -0400

    Changes in reservation card

--- src/components/Cards/Reservation/ReservationCard.jsx
+++ src/components/Cards/Reservation/ReservationCard.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import axios from 'axios';
 import {useNavigate} from "react-router-dom"
 import Images from '../../../data/images'
+import '../Reservation/ReservationCard.css'
+
 import {
     BrowserRouter as Router,
     Switch,
@@ -23,15 +25,14 @@ function ReservationCard(props) {
     <div id='propertyCard' class="card">
     <img class="card-img-top" src={selected[0]} alt="Property" id='eventImage' />
     <div class="card-body">
-    <Link className='titleLink' to={`/property/${props.propertyID}`}>Stay for 3 nights</Link>
+    <Link className='titleLink' to={`/property/${props.propertyID}`}>View Property</Link>
       <p class="card-text"></p>
      <span>
-       <img src='images/favorite.png' alt='favicon' id='favicon'/>
-        <span id='ratingText'>{props.checkInDate}</span>
-        <span id='upvotesText'>({props.checkOutDate})</span>
-        <span id='locationText'>{props.guests}</span>
+        <span id='ratingText'><label><img className='reservationSymbols' alt='checkin' src='https://img.icons8.com/ios/344/clock--v1.png'/> Check-In Date: </label>&nbsp;&nbsp;{props.checkInDate}<br/></span>
+        <span id='upvotesText'><label><img className='reservationSymbols' alt='checkin' src='https://img.icons8.com/ios/344/clock--v3.png'/> Checkout Date:  </label>&nbsp;&nbsp;{props.checkOutDate}<br/></span>
+        <span id='locationText'><label><img className='reservationSymbols' alt='checkin' src='https://cdn-icons.flaticon.com/png/512/641/premium/641132.png?token=exp=1651270048~hmac=e11153b53ee0d79a66ea4bf59ecb06f5'/> Number of Guests:  </label>&nbsp; {props.guests}</span>
      </span>
-    
+     <br/><br/>
       <p><b>Total Cost ${props.price}</b></p>
      <hr/>
       <span>
