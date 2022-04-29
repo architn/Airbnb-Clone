7b0453f48a11ba11e25c198b35d5e0efbf26f4bd
AishwaryaBalyaya10 <balyaya.a@northeastern.edu>
2022-04-29 16:18:22 -0400

    Admin View UI changes

--- src/Views/AdministratorView/AdminViewPropertyUI.jsx
+++ src/Views/AdministratorView/AdminViewPropertyUI.jsx
@@ -2,22 +2,41 @@ import React from 'react'
 import axios from 'axios'
 
 function AdminViewPropertyUI(props) {
+  const styleObj = {
+    fontSize: 14,
+    color: "#4a54f1",
+    textAlign: "center",
+    paddingTop: "100px",
+}
 
     function deleteProperty(){
         axios.delete(`http://localhost:3002/deleteProperty/${props.id}`, {withCredentials: true})
         .then( (response) => {
           if(response.status === 201){
             alert('Successfully deleted!')
+            window.location.reload(true);
           }
         })
     }
   return (
     <div>
-         <p>{props.title}</p>
+         {/* <p>{props.title}</p>
         <p>{props.address}</p>
         <p>{props.city}, {props.state}, {props.country}</p>
         <p>{props.cost}</p>
-        <p><button className='btn btn-danger' onClick={deleteProperty}>DELETE</button></p>
+        <p><button className='btn btn-danger' onClick={deleteProperty}>DELETE</button></p> */}
+        <table className="table table-responsive-sm">
+        <tbody>
+        <tr>
+        <td style={{width:300,style:"italic"}}>{props.title}</td>
+        <td style={{width:250, }}>{props.city}</td>
+        <td style={{width:250}}>{props.state}</td>
+        <td style={{width:250}}>{props.country}</td>
+        <td style={{width:250}}>{props.cost}</td>
+        <td ><button className='btn btn-danger'onClick={deleteProperty}>DELETE</button></td>
+        </tr>
+        </tbody>
+        </table>
     </div>
   )
 }
--- src/Views/AdministratorView/AdminViewUserUI.jsx
+++ src/Views/AdministratorView/AdminViewUserUI.jsx
@@ -1,7 +1,13 @@
 import axios from 'axios'
 import React from 'react'
+import '../../Views/AdministratorView/AdminViewUserUI.css'
+
+//import { useNavigate } from "react-router-dom";
+
+import Table from 'react-bootstrap/Table'
 
 function AdminViewUserUI(props) {
+  //let navigate = useNavigate();
 
     function deleteUser(){
         axios.delete(`http://localhost:3002/deleteUser/${props.id}`, {withCredentials: true})
@@ -12,10 +18,20 @@ function AdminViewUserUI(props) {
         })
     }
   return (
-    <div>
-        <p>{props.name}</p>
+    <div className="container">
+        {/* <p>{props.name}</p>
         <p>{props.email}</p>
-        <p><button className='btn btn-danger' onClick={deleteUser}>DELETE</button></p>
+        <p><button className='btn btn-danger' onClick={deleteUser}>DELETE</button></p> */}
+        <table className="table table-responsive-sm">
+         
+        <tbody>
+        <tr>
+        <td style={{width:400}}>{props.name}</td>
+        <td style={{width:500}}>{props.email}</td>
+        <td ><button className='btn btn-danger' onClick={deleteUser}>DELETE</button></td>
+        </tr>
+        </tbody>
+        </table>
     </div>
   )
 }
